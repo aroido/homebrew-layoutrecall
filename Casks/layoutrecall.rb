497cef37ee78b4f22c9afe96b1032f8969824f70
aroido-bigcat <bigcat@aroido.com>
2026-04-01 13:05:12 +0900

    chore: sync layoutrecall cask for v0.1.3

--- Casks/layoutrecall.rb
+++ Casks/layoutrecall.rb
@@ -1,8 +1,8 @@
 cask "layoutrecall" do
-  version "0.1.2"
-  sha256 "3e97b508e43f79c0855c412ef2fcd458d9f83a7e9b48b22b2dd8cd828477c137"
+  version "0.1.3"
+  sha256 "f20f465bcb8bb09d1ea67db207321a1800e3fe07d59f502120a04a03369f01a9"
 
-  url "https://github.com/aroido/layoutrecall/releases/download/v0.1.2/LayoutRecall-0.1.2.dmg"
+  url "https://github.com/aroido/layoutrecall/releases/download/v0.1.3/LayoutRecall-0.1.3.dmg"
   name "LayoutRecall"
   desc "Restore saved display layouts when identical monitors get scrambled"
   homepage "https://github.com/aroido/layoutrecall"
